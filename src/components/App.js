7f3d92036b4345e46565d3caeea48193b8430683
Abdulhamid <abdulhamidhersi@gmail.com>
2024-11-12 14:42:16 +0300

    comp

--- src/components/App.js
+++ src/components/App.js
@@ -17,16 +17,28 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
-      <NavBar />
+   <NavBar></NavBar>
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
+  
     </div>
   );
 }
+
+
 
 export default App;
